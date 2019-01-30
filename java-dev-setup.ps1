9f25b2580823427d1d8dfe3f56cba323f78fdbfb
Thor Khashayar Salehi <Thor@asgard.pm>
2019-01-30 10:58:17 +0100

    Add files via upload

---
+++ java-dev-setup.ps1
@@ -0,0 +1,12 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install git -y
+choco install cmder -y
+choco install nodejs -y
+choco install yarn  -y
+choco install intellijidea-ultimate -y
+choco install sublimetext3 -y
+choco install notepadplusplus  -y
+choco install jdk8 -y
+choco install robo3t -y
+choco install python -y
+choco install docker -y
