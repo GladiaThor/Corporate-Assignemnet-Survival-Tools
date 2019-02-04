a979928f08d5dbb2f18d8e88733163457c09caf8
Thor Khashayar Salehi <Thor@asgard.pm>
2019-02-04 13:14:18 +0100

    Create git-conf.sh

---
+++ git-conf.sh
@@ -0,0 +1,25 @@
+!#/bin/bash
+echo full name
+read name
+echo email
+read mail
+git config --global user.name "$name"
+git config --global user.email "$mail"
+git config --global core.filemode false
+git config --global core.whitespace cr-at-eol
+
+echo proxy address Leave blank if none
+read proxy
+if [ -z "$proxy" ]; then
+        git config --global http.proxy $proxy
+fi
+
+echo ssh-key passphrase
+read pass
+echo username on this WINDOWS PC
+read uname
+
+ssh-keygen -t rsa -C $mail -N pass -f "C:/Users/$uname/.ssh/id_rsa"
+
+echo ssh-key generated at: "C:/Users/$uname/.ssh/id_rsa"
+clip < C:/Users/$uname/.ssh/id_rsa.pub
