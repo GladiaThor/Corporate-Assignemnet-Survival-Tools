53f6805c9c8a5f3d292bd8ab6f1996a4cb00fac0
Thor Khashayar Salehi <Thor@asgard.pm>
2019-02-04 12:24:35 +0100

    Update and rename java-dev-setup.ps1 to java-dev-env-for-windows.ps1

--- java-dev-env-for-windows.ps1
+++ java-dev-env-for-windows.ps1
@@ -1,4 +1,4 @@
-﻿Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install git -y
 choco install cmder -y
 choco install nodejs -y
@@ -7,6 +7,9 @@ choco install intellijidea-ultimate -y
 choco install sublimetext3 -y
 choco install notepadplusplus  -y
 choco install jdk8 -y
+choco install jdk8 -y
+choco install maven -y
 choco install robo3t -y
 choco install python -y
 choco install docker -y
+choco install slack -y
