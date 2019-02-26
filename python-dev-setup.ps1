4321757d14f2c10e37ce2509e4542d993da63392
Thor Khashayar Salehi <Thor@asgard.pm>
2019-02-26 11:23:59 +0100

    Update python-dev-setup.ps1

--- python-dev-setup.ps1
+++ python-dev-setup.ps1
@@ -3,7 +3,6 @@ if (-not (Test-Path $cinst) -or -not (Test-Path $choco)) {
     Write-Output "Chocolatey was not found at $chocolateyBin. Installing Chocolatey"
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-ForEach ($ChocolateyPackageId in $Packages)
-{
+ForEach ($ChocolateyPackageId in $Packages){
     choco install $ChocolateyPackageId -y
 }
