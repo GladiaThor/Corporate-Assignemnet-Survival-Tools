7c698402b9729ab9224e51003ccb9b3cba265983
Thor Khashayar Salehi <Thor@asgard.pm>
2019-02-26 11:42:24 +0100

    Update python-dev-env-for-windows.ps1

--- python-dev-env-for-windows.ps1
+++ python-dev-env-for-windows.ps1
@@ -1,10 +1,9 @@
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-
-{   
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){   
 $arguments = "& '" + $myinvocation.mycommand.definition + "'"
 Start-Process powershell -Verb runAs -ArgumentList $arguments
 Break
 }
+
 $chocolateyBin = [Environment]::GetEnvironmentVariable("ChocolateyInstall", "Machine") + "\bin"
 $cinst = "$chocolateyBin\cinst.exe"
 $choco = "$chocolateyBin\choco.exe"
