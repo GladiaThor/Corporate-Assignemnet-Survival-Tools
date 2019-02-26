cc93baf6bc5af7dff23bc9ae94a47c4f72b13bbf
Thor Khashayar Salehi <Thor@asgard.pm>
2019-02-26 11:23:31 +0100

    Update python-dev-setup.ps1

--- python-dev-setup.ps1
+++ python-dev-setup.ps1
@@ -1,4 +1,4 @@
-$Packages =  'git','cmder','intellijidea-ultimate','sublimetext3','notepadplusplus','robo3t','python'
+$Packages =  'git','cmder','intellijidea-ultimate','sublimetext3','notepadplusplus','python'
 if (-not (Test-Path $cinst) -or -not (Test-Path $choco)) {
     Write-Output "Chocolatey was not found at $chocolateyBin. Installing Chocolatey"
     Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
