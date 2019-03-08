cf4da0b033463a8539d9bd0e61dd95c7e67c6e69
Thor Khashayar Salehi <Thor@asgard.pm>
2019-03-08 10:00:12 +0100

    Update java-dev-env-for-windows.ps1

--- java-dev-env-for-windows.ps1
+++ java-dev-env-for-windows.ps1
@@ -1,15 +1,18 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install git -y
-choco install cmder -y
-choco install nodejs -y
-choco install yarn  -y
-choco install intellijidea-ultimate -y
-choco install sublimetext3 -y
-choco install notepadplusplus  -y
-choco install jdk8 -y
-choco install jdk11 -y
-choco install maven -y
-choco install robo3t -y
-choco install python -y
-choco install docker-desktop -y
-choco install slack -y
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
+
+$chocolateyBin = [Environment]::GetEnvironmentVariable("ChocolateyInstall", "Machine") + "\bin"
+$cinst = "$chocolateyBin\cinst.exe"
+$choco = "$chocolateyBin\choco.exe"
+$Packages =  'git', 'cmder', 'nodejs', 'yarn',  'intellijidea-ultimate','sublimetext3', 'notepadplusplus',  'jdk8', 'jdk11', 'maven', 'robo3t', 'python', 'docker-desktop', 'slack'
+if (-not (Test-Path $cinst) -or -not (Test-Path $choco)) {
+    Write-Output "Chocolatey was not found at $chocolateyBin. Installing Chocolatey"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+ForEach ($ChocolateyPackageId in $Packages){
+    choco install $ChocolateyPackageId -y
+}
